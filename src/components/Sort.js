b21364a5a88fb7cc90cba313da92b736298c3131
soumikm.official@gmail.com <soumikmofficial@gmail.com>
2021-11-20 01:37:32 +0530

    sort component with no functionality

--- src/components/Sort.js
+++ src/components/Sort.js
@@ -1,10 +1,42 @@
-import React from 'react'
-import { useFilterContext } from '../context/filter_context'
-import { BsFillGridFill, BsList } from 'react-icons/bs'
-import styled from 'styled-components'
+import React from "react";
+import { useFilterContext } from "../context/filter_context";
+import { BsFillGridFill, BsList } from "react-icons/bs";
+import styled from "styled-components";
 const Sort = () => {
-  return <h4>sort </h4>
-}
+  const { filtered_products: products, grid_view } = useFilterContext();
+
+  return (
+    <Wrapper>
+      <div className="btn-container">
+        <button
+          type="button"
+          className={grid_view ? "active" : null}
+          // onClick={() => dispatch({ type: SET_GRIDVIEW })}
+        >
+          <BsFillGridFill />
+        </button>
+        <button
+          type="button"
+          className={!grid_view ? "active" : null}
+          // onClick={() => dispatch({ type: SET_LISTVIEW })}
+        >
+          <BsList />
+        </button>
+      </div>
+      <p>{products.length} products were found</p>
+      <hr />
+      <form>
+        <label htmlFor="sort">sort by</label>
+        <select name="sort" id="sort" className="sort-input">
+          <option value="price-lowest">price (lowest)</option>
+          <option value="price-highest">price (highest)</option>
+          <option value="name-a">name (a-z)</option>
+          <option value="name-z">name (z-a)</option>
+        </select>
+      </form>
+    </Wrapper>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -66,7 +98,8 @@ const Wrapper = styled.section`
   label {
     font-size: 1rem;
     text-transform: capitalize;
+    margin-right: 0.5rem;
   }
-`
+`;
 
-export default Sort
+export default Sort;
