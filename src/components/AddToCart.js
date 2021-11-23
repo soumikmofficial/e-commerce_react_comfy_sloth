1239191aa04d926e3fd4b9138142f09c141a7aca
soumikm.official@gmail.com <soumikmofficial@gmail.com>
2021-11-24 00:24:04 +0530

    logic initiation for adding new product to cart

--- src/components/AddToCart.js
+++ src/components/AddToCart.js
@@ -7,6 +7,7 @@ import AmountButtons from "./AmountButtons";
 
 const AddToCart = ({ product }) => {
   const { id, stock, colors } = product;
+  const { addToCart } = useCartContext();
 
   const [mainColor, setMainColor] = useState(colors[0]);
   const [amount, setAmount] = useState(1);
@@ -48,7 +49,11 @@ const AddToCart = ({ product }) => {
           increaseAmount={increaseAmount}
           decreaseAmount={decreaseAmount}
         />
-        <Link to="/cart" class="btn">
+        <Link
+          to="/cart"
+          className="btn"
+          onClick={() => addToCart(id, mainColor, amount, product)}
+        >
           add to cart
         </Link>
       </div>
--- src/reducers/cart_reducer.js
+++ src/reducers/cart_reducer.js
@@ -4,11 +4,29 @@ import {
   COUNT_CART_TOTALS,
   REMOVE_CART_ITEM,
   TOGGLE_CART_ITEM_AMOUNT,
-} from '../actions'
+} from "../actions";
 
 const cart_reducer = (state, action) => {
-  return state
-  throw new Error(`No Matching "${action.type}" - action type`)
-}
+  if (action.type === ADD_TO_CART) {
+    const { id, amount, color, product } = action.payload;
+    const tempProduct = state.cart.find((p) => p.id === id + color);
+    if (tempProduct) {
+    } else {
+      console.log(product);
 
-export default cart_reducer
+      const newProduct = {
+        color,
+        amount,
+        price: product.price,
+        id: id + color,
+        name: product.name,
+        max: product.stock,
+        image: product.images[0].url,
+      };
+      return { ...state, cart: [...state.cart, newProduct] };
+    }
+  }
+  throw new Error(`No Matching "${action.type}" - action type`);
+};
+
+export default cart_reducer;
